0fedda91ee3fe8ed8ae24b40f5397dcae9b38cda
Jonas S Karlsson <jsk@yesco.org>
2026-07-02 21:22:28 +0800

    memory stuff

--- zup.c
+++ zup.c
@@ -16,6 +16,34 @@ char op, four;
 
 uint32_t tmp, C;
 
+word memw[32*1024]; // hibyte: odd byteaddr
+
+char bread(word a) {
+  word w= memw[a/2];
+  if (a & 1) return w>>8; 
+  return w;
+}
+
+void bwrite(word a, char b) {
+  word w= memw[a/2];
+  if (a & 1) w= (w & 0xff)   | (b << 8);
+  else       w= (w & 0xff00) | b;
+  memw[a/2]= w;
+}
+
+// if sign bit set: operate on byte
+word memread(word tos, char four, word index) {
+  word addr= ((tos << 4) | four) + index;
+  if (((int16_t)tos) < 0) return bread(addr);
+  else         return memw[addr];
+}
+
+// if sign bit set: operate on byte
+void memwrite(int16_t tos, char four, word index, word val) {
+  word addr= ((tos << 4) | four) + index;
+  if (((int16_t)tos) < 0) bwrite(addr, val);
+  else         memw[addr]= val;
+}
 
 void push(word w) {
   *sp++ = nos2;
@@ -56,8 +84,11 @@ int main() {
 
   op= 42;
   
-  C= !!C; // normalize => 0 or 1
-  four= op & 0xF;
+ next:
+  op= bread(pc);
+  
+  C= !!C; // normalize Carry => 0 or 1
+  four= op & 0xF; // get lowest four bits
   
   switch (op) {
 
@@ -65,34 +96,57 @@ int main() {
   case 0x00 ... 0x3F: {
     if (!prefix) push(0);
     tos= (tos<<6) ^ (op ^ 63);
-    break; }
+    ++prefix;
+    goto isprefix; }
 
 
-    // == 16 READ/WRITES (write / barts)
-
-    // WRTE: CASE prefix_count OF
-  case 0x50 ... 0x5F: {
-    switch (prefix) {
-
-      //     0 (1 Byte)  : REGISTER. Addresses global slots 0 ... 15 directly. 
-      //                   Address = { 11'b000000000000, ir[3:0] } 
-    case 0: break;
-
-      //     1 (2 Bytes) : 10 bits address read/write. Range 0-1023. 
-      //                   Address = { 0….0,   tos[5:0], ir[3:0] }
-    case 1: break;
-
-      //     2 (3 Bytes) : Absolute Far Write. Full 16-bit address. 
-      //                   Address = {   tos[11:0],      ir[3:0] }
-    case 2: break;
-
-    }
-      
-    break; }
+    // == 16+16 READ/WRITES (write / barts)
     //   * SIGN BIT: WORD / BYTE ADDRESSING using indexed slots 
     //       0 : WORD INDEX 15-bit hardware words. 64KB 
     //       1 : BYTE INDEX 15-bit bytes.          32KB 
     //     NOTE: bytes can only address half the memory (lower); baddr=waddr*2 
+
+  case 0x40 ... 0x4F: {
+    // READ
+
+    // CASE prefix_count OF
+    switch (prefix) {
+
+      //     0 (1 Byte)  : REGISTER. Addresses global slots 0 ... 15 directly. 
+      //                   Address = { 11'b000000000000, ir[3:0] } 
+    case 0: push(memw[four]); break;
+
+      //     1 (2 Bytes) : 10 bits address read/write. Range 0-1023. 
+      //                   Address = { 0….0,   tos[5:0], ir[3:0] }
+      //     2 (3 Bytes) : Absolute Far Write. Full 16-bit address. 
+      //                   Address = {   tos[11:0],      ir[3:0] }
+    case 1:
+    case 2: tmp= pop(); push(memread(tmp, four, 0)); break;
+
+    }
+      
+    break; }
+
+  case 0x50 ... 0x5F: {
+    // WRITE
+
+    // CASE prefix_count OF
+    switch (prefix) {
+
+      //     0 (1 Byte)  : REGISTER. Addresses global slots 0 ... 15 directly. 
+      //                   Address = { 11'b000000000000, ir[3:0] } 
+    case 0: memw[four]= pop(); break;
+
+      //     1 (2 Bytes) : 10 bits address read/write. Range 0-1023. 
+      //                   Address = { 0….0,   tos[5:0], ir[3:0] }
+      //     2 (3 Bytes) : Absolute Far Write. Full 16-bit address. 
+      //                   Address = {   tos[11:0],      ir[3:0] }
+    case 1:
+    case 2: tmp= pop(); memwrite(tmp, four, 0, tos); break;
+
+    }
+      
+    break; }
    
 
     // 16 SUBROUTINES (jsr)
@@ -103,10 +157,19 @@ int main() {
       //     0 (1 Byte)  : Fast Vector JSR# (0-15) to Top of Memory, 8 byte slots, 
       //                   Address = { 9'b111111111, ir[3:0], 3'b000 } 
       //        SPECIAL VECTORS  
-      //          0 : JSRTOS  - Jump SubRoutine to TOS. Method dispatch. 
-      //          1 : BEGIN   - Begin loop; inject PC+1 into R2, push old R2. 
-      //          i : USER#   - 2-15: USER DEFINED single byte code instructions! 
-    case 0: jsr(0b1111111110000000 + 8*four); break;
+    case 0:
+      switch (op) {
+
+	//          0 : JSRTOS  - Jump SubRoutine to TOS. Method dispatch. 
+      case 0x60: jsr(pop()); break;
+
+	//          1 : BEGIN   - Begin loop; inject PC+1 into R2, push old R2. 
+      case 0x61: push(R); R2= pc+1; break;
+
+	//          i : USER#   - 2-15: USER DEFINED single byte code instructions! 
+      defaalt: jsr(0b1111111110000000 + 8*four); break;
+
+      }
 
       //     1 (2 Bytes) : Absolute Near JSR (Replaces lower 10 bits of PC). 
       //                   Address = { PC[15:10], tos[5:0], ir[3:0] } 
@@ -161,29 +224,74 @@ int main() {
 
     // 32 CONDITIONAL RETURNS/BRANCHES 
   case 0x80 ... 0x9F: {
-    break; }
-    // {100 n ccdd} 
-    //   1 negation bit 
-    //   2 Bits Condition [Z Minus Carry Odd] 
-    //   2 Bits Displacement [-16  0 +16 +22] 
-    //   Effective address = <R[15:6]+displacement> <tos 6 bits> 
 
-    // 0 prefix      : %ret      - jump Be ACK from JSR on condition. 
-    // 1 prefix      : %branch   - window absolute jump if true. 
-    // 2 prefix 80-8F: iread     - indexed read (addr+tos) => val. 
-    //          90-9F: iwrite    - write nos to (addr+tos) => nos. 
+    if (prefix < 2) {
+      // {100 n ccdd} 
+      //   1 negation bit 
+      //   2 Bits Condition [Z Minus Carry Odd] 
+      //   2 Bits Displacement [-16  0 +16 +22] 
+      //   Effective address = <R[15:6]+displacement> <tos 6 bits> 
 
-    // % conditions: 
-    // 0x80 ... 0x83    : z*        - Jump If     Zero (Z)           
-    // 0x84 ... 0x87    : c*        - Jump If     Carry (C)   
-    // 0x88 ... 0x8B    : m*        - Jump If     Minus (Sign bit)        
-    // 0x8C ... 0x8F    : o*        - Jump If     Odd  (Lowest Bit)  
-    // 0x90 ... 0x93    : nz*       - Jump If NOT Zero (NZ)     
-    // 0x94 ... 0x97    : nc*       - Jump If NOT Carry (NC)   
-    // 0x98 ... 0x9B    : p*        - Jump If NOT Minus (NS) = Positive (P) 
-    // 0x9C ... 0x9F    : e*        - Jump If NOT Odd (Even)  
+      // % conditions: 
+      // 0x80 ... 0x83    : z*        - Jump If     Zero (Z)           
+      // 0x84 ... 0x87    : c*        - Jump If     Carry (C)   
+      // 0x88 ... 0x8B    : m*        - Jump If     Minus (Sign bit)        
+      // 0x8C ... 0x8F    : o*        - Jump If     Odd  (Lowest Bit)  
 
+      // 0x90 ... 0x93    : nz*       - Jump If NOT Zero (NZ) 
+      // 0x94 ... 0x97    : nc*       - Jump If NOT Carry (NC)   
+      // 0x98 ... 0x9B    : p*        - Jump If NOT Minus (NS) = Positive (P) 
+      // 0x9C ... 0x9F    : e*        - Jump If NOT Odd (Even)  
 
+      char neg= op & 0b10000;
+      char disp= 16 * ((op & 0b11)-1), yes;
+      word addr= (pc & 0b1111111111000000) + disp + (tos & 0b111111);
+
+      // jump?
+      switch (op & 0b1100) {
+      case 0b0000: yes= !tos; break;
+      case 0b0100: yes= (tos<0); break;
+      case 0b1000: yes= C; break;
+      case 0b1100: yes= tos&1; break;
+      }
+      if (neg) yes= !yes;
+      if (!yes) break;
+      
+      // 0 prefix      : %ret      - jump Be ACK from JSR on condition. 
+      // 1 prefix      : %branch   - window absolute jump if true. 
+      if (prefix) {
+
+	// window jump
+	pc= addr;
+
+      } else {
+
+	// return
+	ret();
+
+      }
+      
+    } else {
+
+      // index read/write
+      // addr= {  tos << 12, four } + nos
+      tmp= pop();
+      
+      if (op <= 0x8f) {
+
+	// 2 prefix 80-8F: iread     - indexed read (addr+tos) => val. 
+	tos= memread(tmp, four, tos);
+	     
+      } else {
+
+	//          90-9F: iwrite    - write nos to (addr+tos) => nos. 
+	memwrite(tmp, four, tos, nos);
+
+	// TODO: leaves index, val on stack (can't pop 2)
+      }
+
+  } break; }
+  
     // 64 FRAME INDEXING OPERANDS (Fast-Path Block)
   case 0xA0 ... 0xDF: {
     break; }
@@ -199,8 +307,9 @@ int main() {
 
     // == REGISTER MANIPULATION & STACK SHORTCUTS (Ode to 6502 Mapping)
 
-  case 0xE0: tos++; break; // inc      - Increment TOS by 1.
-  case 0xE1: tos--; break; // dec      - Decrement TOS by 1.
+  case 0xE0: ++tos; break; // inc      - Increment TOS by 1.
+  case 0xE1: --tos; break; // dec      - Decrement TOS by 1.
+
     // ror      - Rotate C into TOS right by 1 bit. Spill to C. 
   case 0xE2: tmp= C; C= tos&1; tos= (tos>>1) + (tmp<<15); break;
     // asr      - Arithmetic shift TOS right 1 bit; spill to C. 
@@ -213,7 +322,9 @@ int main() {
   case 0xE6: C= tos&(1<<15); tos<<= 1; break;
     // shl4     - Shift TOS left by 4 bits (Nibble shift); C. 
   case 0xE7: C= tos&(1<<12); tos<<= 4; break;
+
     //TODO: 0xE8          : rand  ?  - Randomization by gaussian. 
+
     // mul      - Multiply16x16=> TOS=High 16-bits, NOS=Low. 
   case 0xE9: tmp= tos*nos; tos= tmp>>16; nos= tmp&0xffff; break;
   case 0xEA: break; // nop      - No Operation; clears LIT_ACTIVE flag (6502).
@@ -227,18 +338,21 @@ int main() {
     // swap     - Exchange the positions of TOS and NOS.
   case 0xEC: tmp= tos; tos= nos; nos= tmp; break;
 
-
   case 0xED: push(nos); break;  // over     - Copy NOS to the top of the stack.
   case 0xEF: push(tos); break;  // dup      - Duplicate TOS.
 
+
     // [0xF0 - 0xF7] : 8 MATH & LOGIC ALU CORE INSTRUCTIONS (a b -> c) 
+
     // add     - C; Push (NOS + TOS) to stack.
   case 0xF0: tmp= pop(); tos= tmp= tmp+tos; C= tmp>0xffff; break;
     // adc     - C; Push (NOS + TOS + C). (negate==-1 xor inc)
   case 0xF1: tmp= pop(); tos= tmp+tos+C; C= tmp>0xffff; break; 
     // sub     - C; Push (NOS - TOS). C=!borrow (sbc==adc sub)
   case 0xF2: tmp= pop(); tos= tmp-tos+C; C= tmp>0xffff; break; 
+
     // TODO: 0xF3          : fma  ?  - Fused Multiply-Add Step. 
+
   case 0xF4: tmp= pop(); tos&= tmp; break; // and     - Push (NOS bitwise AND TOS). 
   case 0xF5: tmp= pop(); tos|= tmp; break; // or      - Push (NOS bitwise OR TOS). 
   case 0xF6: tmp= pop(); tos^= tmp; break; // xor     - Push (NOS bitwise XOR TOS). (not==-1 xor) 
@@ -252,10 +366,19 @@ int main() {
   case 0xFB: push(fp); fp= rpop(); break; // fcopy   - Drop R. Push frame on data stack. 
   case 0xFC: fp= pop(); break; //         // fset    - Frame register set. 
 
-  case 0xFD: tos= (tos<<8) | (tos>>8); break; //  bswap   - Swaps bytes.
-  case 0xFE: tos= -(signed) tos; break;       //  sign    - Flips sign bit.
-  case 0xFF: push(-1); break;                 //  -1      - Minus one, aka True.
+  case 0xFD: tos= (tos<<8) | (tos>>8); break;   //  bswap   - Swaps bytes.
+  case 0xFE: tos= tos ^ (1<<15); goto isprefix; //  sign    - Flips sign bit.
+  case 0xFF: push(-1); goto isprefix;           //  -1      - Minus one, aka True.
   }
+
+  // most instructions reset prefix, unless they are modifier (sh6l# sign -1)
+  prefix= 0;
+  
+ isprefix:
+  ++pc;
+  
+  goto next;
+
   return 0;
 }
 
