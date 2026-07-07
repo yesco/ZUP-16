fc4692d6d90912c287d59090cb95f14c9abe4a4c
Jonas S Karlsson <jsk@yesco.org>
2026-07-07 18:13:41 +0800

    iw8: 55 LUT? z bit in the middle

--- MinimalCPU/iw8.v
+++ MinimalCPU/iw8.v
@@ -52,36 +52,36 @@ module iw8 (
       // BITS: { instr, drop, push, ?, ? ... }
       casez ({op[7:4], !n, op[3:0]})
 	// -- LITERAL "8 bits" (hi=0, do INV!)
-	8'b0???????: T= op;
+	9'b0????_?_????: T= op;
 
 	// -- DROPPERS: (a b -> c)
-	8'b110??_000: T= t + n;     // + 
-	8'b110??_001: T= t & n;     // AND
-	8'b110??_010: T= t | n;     // OR
-	8'b110??_011: T= t ^ n;     // XOR
-//	8'b110??_100: ram[t]= n;    // !    (value remain)
-	8'b110??_101: R= t;         // !R
-	8'b110?1_110: PC= t;        // GOTO
-//	8'b110??_100: if (z) PC= t; // %BRANCH
-//	8'b110??_100: ;             // ZBRANCH (handle outside)
-//	8'b110??_111: ;             // DROP
+	9'b110??_?_000: T= t + n;     // + 
+	9'b110??_?_001: T= t & n;     // AND
+	9'b110??_?_010: T= t | n;     // OR
+	9'b110??_?_011: T= t ^ n;     // XOR
+//	9'b110??_100: ram[t]= n;    // !    (value remain)
+	9'b110??_?_101: R= t;         // !R
+	9'b110?1_1_110: PC= t;        // GOTO (Bit 4=1 AND !n=1: execute jump)
+//	9'b110??_100: if (z) PC= t; // %BRANCH
+//	9'b110??_100: ;             // ZBRANCH (handle outside)
+//	9'b110??_111: ;             // DROP
 //                    T= t;         // NIP (no space!)
 
 	// -- REGISTER: (a b -> c d)
-	8'b100??_000: T= ~t;        // INV
-	8'b100??_001: T= t<<1;      // SHL
-	8'b100??_010: T= t>>1;      // SHR
-//	8'b101??_011: T= ram[t]     // @
-	8'b101??_111:
+	9'b100??_?_000: T= ~t;        // INV
+	9'b100??_?_001: T= t<<1;      // SHL
+	9'b100??_?_010: T= t>>1;      // SHR
+//	9'b101??_011: T= ram[t]     // @
+	9'b101??_?_111:
 	  begin T= n; N= t; end     // SWAP
 	
 
 	// -- PRODUCER: (a b -> c d e)
-	8'b101??_000: ;             // DUP
-	8'b101??_001: T= r;         // @R
-//	8'b101??_000: T= n;         // OVER
-//	8'b101??_000: N2= t;        // TUCK
-	8'b101??_010: T= pc;        // ???
+	9'b101??_?_000: ;             // DUP
+	9'b101??_?_001: T= r;         // @R
+//	9'b101??_000: T= n;         // OVER
+//	9'b101??_000: N2= t;        // TUCK
+	9'b101??_?_010: T= pc;        // ???
 
 	// -- SPECIAL: 111_xx_xxx
 
