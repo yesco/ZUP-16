0614c16113ac64f2ad96f0f5941aa21f9196bfc0
Jonas S Karlsson <jsk@yesco.org>
2026-07-07 18:53:20 +0800

    simplify

--- MinimalCPU/iw8.v
+++ MinimalCPU/iw8.v
@@ -20,7 +20,7 @@ module iw8 (
    // ==========================================================
    // New state use capitals
    reg [7:0]  pc, t, n, r, T, N, R;
-   wire       n_sign;
+//   wire       n_sign;
    
    // Testing on N for jump because T is destination!
 //   assign z= (t == 0); // LUT: 183
@@ -50,39 +50,39 @@ module iw8 (
       endcase
        
       // BITS: { !n (1 bit), op[7:0] (8 bits) } = 9 bits total
-      casez ({!n, op})
+      casez ({op})
 	// -- LITERAL "8 bits" (hi=0, do INV!)
-	9'b?_0???????: T= op;
+	8'b0????_???: T= op;
 
 	// -- DROPPERS: (a b -> c)
-	9'b?_110??_000: T= t + n;     // + 
-	9'b?_110??_001: T= t & n;     // AND
-	9'b?_110??_010: T= t | n;     // OR
-	9'b?_110??_011: T= t ^ n;     // XOR
-//	9'b?_110??_100: ram[t]= n;    // !    (value remain)
-	9'b?_110??_101: R= t;         // !R
-	9'b?_11010_110: PC= t;        // GOTO (Bit 4=1: Always jumps regardless of !n flag state)
-	9'b1_11000_110: PC= t;        // %BRANCH (Bit 4=0 AND !n=1: Conditional Jump executes)
-//	9'b110??_100: if (z) PC= t; // %BRANCH
-//	9'b110??_100: ;             // ZBRANCH (handle outside)
-	9'b?_110??_111: ;             // DROP
+	8'b110??_000: T= t + n;     // + 
+	8'b110??_001: T= t & n;     // AND
+	8'b110??_010: T= t | n;     // OR
+	8'b110??_011: T= t ^ n;     // XOR
+//	8'b?_110??_100: ram[t]= n;    // !    (value remain)
+	8'b110??_101: R= t;         // !R
+//	8'b110??_110: PC= t;        // GOTO (Bit 4=1: Always jumps regardless of !n flag state)
+//	8'b110??_111: PC= t;        // %BRANCH (Bit 4=0 AND !n=1: Conditional Jump executes)
+	8'b110??_110: if (!n) PC= t; // %BRANCH
+//	8'b110??_100: ;             // ZBRANCH (handle outside)
+	8'b110??_111: ;             // DROP
 //                    T= t;         // NIP (no space!)
 
 	// -- REGISTER: (a b -> c d)
-	9'b?_100??_000: T= ~t;        // INV
-	9'b?_100??_001: T= t<<1;      // SHL
-	9'b?_100??_010: T= t>>1;      // SHR
-//	9'b?_101??_011: T= ram[t]     // @
-	9'b?_101??_111:
+	8'b100??_000: T= ~t;        // INV
+	8'b100??_001: T= t<<1;      // SHL
+	8'b100??_010: T= t>>1;      // SHR
+//	8'b101??_011: T= ram[t]     // @
+	8'b101??_111:
 	  begin T= n; N= t; end     // SWAP
 	
 
 	// -- PRODUCER: (a b -> c d e)
-	9'b?_101??_000: ;             // DUP
-	9'b?_101??_001: T= r;         // @R
-//	9'b?_101??_000: T= n;         // OVER
-//	9'b?_101??_000: N2= t;        // TUCK
-	9'b?_101??_010: T= pc;        // ???
+	8'b101??_000: ;             // DUP
+	8'b101??_001: T= r;         // @R
+//	8'b101??_000: T= n;         // OVER
+//	8'b101??_000: N2= t;        // TUCK
+	8'b101??_010: T= pc;        // ???
 
 	// -- SPECIAL: 111_xx_xxx
 
