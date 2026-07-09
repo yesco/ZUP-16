af47c9886e99f4945dc9a556f3b647f2bf77ae90
Jonas S Karlsson <jsk@yesco.org>
2026-07-09 12:50:54 +0800

    zup 8-bit op casez

--- MinimalCPU/zup.v
+++ MinimalCPU/zup.v
@@ -40,49 +40,49 @@ module zup #(
         // Baseline global hardware defaults
         T= t; N= n; C= 0; sd= 0; rd= 0; a= n; b= t; cin= 0; PC= pc;
  
-        if (!is_op) begin
+        if (!is_op) begin // Cheaper with if!
             T= op; sd= 1; // Literal flag processing
         end else begin
-            case (key)
+            casez (op) // Cheaper with full opo (-20 LUT)!
                 // --- GROUP 0: Two-Argument Reductions (224-231) ---
-                5'b00000: begin sd=-1; {C,T}= sum; a= n; b= t; cin=0; end // add
-                5'b00001: begin sd=-1; {C,T}= sum; a= n; b= t; cin=c; end // adc
-                5'b00010: begin sd=-1; {C,T}= sum; a= n; b=~t; cin=1; end // sub
-                5'b00011: begin sd=-1;     T= t;                      end // nip
-                5'b00100: begin sd=-1;     T= n & t;                  end // and
-                5'b00101: begin sd=-1;     T= n | t;                  end // or
-                5'b00110: begin sd=-1;     T= n ^ t;                  end // xor
-                5'b00111: begin sd=-1;     T= n;                      end // drop
+                8'b11100000: begin sd=-1; {C,T}= sum; a= n; b= t; cin=0; end // add
+                8'b11100001: begin sd=-1; {C,T}= sum; a= n; b= t; cin=c; end // adc
+                8'b11100010: begin sd=-1; {C,T}= sum; a= n; b=~t; cin=1; end // sub
+                8'b11100011: begin sd=-1;     T= t;                      end // nip
+                8'b11100100: begin sd=-1;     T= n & t;                  end // and
+                8'b11100101: begin sd=-1;     T= n | t;                  end // or
+                8'b11100110: begin sd=-1;     T= n ^ t;                  end // xor
+                8'b11100111: begin sd=-1;     T= n;                      end // drop
           
                 // --- GROUP 1: Control Flow & Inversions (232-239) ---
-                5'b01000: begin     T=~t;                       end // not
-                5'b01001: begin {C,T}= sum; a= 0; b=~t; cin= 1; end // neg
-                5'b01010: begin     T= t;                       end // nop
-                5'b01011: begin     T= n;                       end // swap
-                5'b01100: begin     T= n;                       end // rot
-                5'b01101: begin sd=-1; if(!n) PC= t;            end // zjp
-                5'b01110: begin sd=-1; if(!c) PC= t;            end // njp
-                5'b01111: begin rd=-1;                          end // ret
+                8'b11101000: begin     T=~t;                       end // not
+                8'b11101001: begin {C,T}= sum; a= 0; b=~t; cin= 1; end // neg
+                8'b11101010: begin     T= t;                       end // nop
+                8'b11101011: begin     T= n;                       end // swap
+                8'b11101100: begin     T= n;                       end // rot
+                8'b11101101: begin sd=-1; if(!n) PC= t;            end // zjp
+                8'b11101110: begin sd=-1; if(!c) PC= t;            end // njp
+                8'b11101111: begin rd=-1;                          end // ret
 
                 // --- GROUP 2: Math Modifiers and Shifters (240-247) ---
-                5'b10000: begin {C,T}= sum; a= 0;    b= t; cin= 1; end // inc
-                5'b10001: begin {C,T}= sum; a= ones; b= t; cin= 0; end // dec
-                5'b10010: begin {T,C}= {   t[W-1],    t };         end // asr
-                5'b10011: begin {T,C}= { t, t[W-1:1], t };         end // ror
-                5'b10100: begin {T,C}= { 1'b0,        t };         end // shr1
-                5'b10101: begin {C,T}= sum; a= t;    b= t; cin= 0; end // shl1
-                5'b10110: begin     T= { 4'b0000,t[W-1:4] }; C= t; end // shr4
-                5'b10111: begin     T= { t[W-5:0],4'b0000 }; C= t; end // shl4
+                8'b11110000: begin {C,T}= sum; a= 0;    b= t; cin= 1; end // inc
+                8'b11110001: begin {C,T}= sum; a= ones; b= t; cin= 0; end // dec
+                8'b11110010: begin {T,C}= {   t[W-1],    t };         end // asr
+                8'b11110011: begin {T,C}= { t, t[W-1:1], t };         end // ror
+                8'b11110100: begin {T,C}= { 1'b0,        t };         end // shr1
+                8'b11110101: begin {C,T}= sum; a= t;    b= t; cin= 0; end // shl1
+                8'b11110110: begin     T= { 4'b0000,t[W-1:4] }; C= t; end // shr4
+                8'b11110111: begin     T= { t[W-5:0],4'b0000 }; C= t; end // shl4
 
                 // --- GROUP 3: Stack Mutation & Return Stack Hooks (248-255) ---
-                5'b11000: begin           {T,N}= mul;                   end // mul
-                5'b11001: begin sd=-1; rd= 1; T= n;                     end // >r
-                5'b11010: begin sd= 1;        T= r;                     end // @r
-                5'b11011: begin sd= 1; rd=-1; T= r;                     end // r>
-                5'b11100: begin sd= 1;        T= t;                     end // tuck
-                5'b11101: begin sd= 1;        T= n;                     end // over
-                5'b11110: begin sd= 1;        T= t;                     end // dup
-                5'b11111: begin sd= 1;        T= ones;                  end // true
+                8'b11111000: begin           {T,N}= mul;                   end // mul
+                8'b11111001: begin sd=-1; rd= 1; T= n;                     end // >r
+                8'b11111010: begin sd= 1;        T= r;                     end // @r
+                8'b11111011: begin sd= 1; rd=-1; T= r;                     end // r>
+                8'b11111100: begin sd= 1;        T= t;                     end // tuck
+                8'b11111101: begin sd= 1;        T= n;                     end // over
+                8'b11111110: begin sd= 1;        T= t;                     end // dup
+                8'b11111111: begin sd= 1;        T= ones;                  end // true
             endcase
         end
     end
