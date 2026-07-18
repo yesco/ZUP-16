4dbe8d826977d2d1751a2fb2689baf9adabeea30
Jonas S Karlsson <jsk@yesco.org>
2026-07-18 20:43:22 +0800

    handle numbers

--- parse.c
+++ parse.c
@@ -1,5 +1,11 @@
 // Parse.c - simple 8byte tokenizer
 
+// --------------------------------------------------
+// Settings
+
+#define NL_TOKEN
+
+
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
@@ -32,37 +38,89 @@ char* readfile(char* filename) {
 #define STEP (*(*s)++)
 
 void skipspc(char** s) {
-  //  while(isspace(**s)) STEP;
+#ifdef NL_TOKEN
   while(isblank(**s)) STEP;
+#else
+  while(isspace(**s)) STEP;
+#endif
 }
 
 #define TRUNC 0xff0000000000000
 
+token lasttok= 0;
+
+// is isbasenum:
+//   0: normal string token
+//   8-16: base #
+//   <0: illegal base #, token==illegal char
+signed char isbasenum= 0; 
+
+// Anything starting with a digit must be a number ala C
+// Any token positive is complete
+// A token with hibit set is truncated
+
 token next(char** s) {
   token t= 0;
   
+  isbasenum= 0;
+  
   skipspc(s);
 
+  if (isdigit(**s)) {
+
+    // base?
+    if (STEP == '0') {
+      switch(tolower(STEP)) {
+      case 'x': case 'h': isbasenum= 16; break;
+      case 'b': isbasenum= 2; break;
+      default: t= lasttok-'0';
+      case 'o': isbasenum= 8; break;
+      }
+    } else isbasenum= 10;
+
+    // read number
+    while (isalnum(**s) || **s == '_') {
+      char d= toupper(STEP)-'0';
+      if (lasttok == '_') continue;
+      // TODO: handle decimals?
+      if (d > 9) d-= 'A'-'9'+10;
+      if (d >= isbasenum) { isbasenum= -isbasenum; return lasttok; }
+      t*= isbasenum; t+= d;
+    }
+
+    return t;
+  }
+  
   // Gobble string (Not UTF-8 safe)
-  while(isalnum(**s)) {
-    if (t & TRUNC) t|= TRUNC;
-    t<<= 7; t|= STEP;
+  while(isalnum(**s) || **s=='_') {
+    if (t & TRUNC) t|= TRUNC | (t>>(64-8));
+    t<<= 7; t^= STEP;
   }
   
   // No string/num return char
-  return t ? t : STEP;
+  return (lasttok= t ? t : STEP);
 }
 
 token prtoken(token t) {
-  char c;
+  switch(isbasenum) {
+  case 2:
+  case 8:
+  case 10:
+  case 16: printf("%x ", t); return t;
+  default: printf("%%%c(0x%2x) ", t, t); return t;
+  case 0: break;
+  }
+
+  // truncated? (hibit set)
   if (((int64_t)t) < 0) putchar('*');
 
+  token o= t;
   while(t) {
-    c= (t>>(64-8)) & 0x7f; // shift down oldest, remove top bit
+    char c= (t>>(64-8)) & 0x7f; // shift down oldest, remove top bit
     if (c) putchar(c);
     t<<= 7;
   }
-  putchar(' ');
+  if (o != 10) putchar(' ');
   return t;
 }
 
@@ -78,12 +136,13 @@ int main() {
   {
     char* s= f;
     token t;
-    while((t= next(&s))) prtoken(t);
+    while((t= next(&s)) || isbasenum) prtoken(t);
     puts("----------------");
   }
 
   free(f);
 }
 
+// Test long symbols
 // abcdefgh abcdefghi abcdefghij abcdefghijk
 
